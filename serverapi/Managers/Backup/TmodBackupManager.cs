9ae75245ea49dac31418696ab3d001bae37c80f7
Robettaja <rompula2005@gmail.com>
2026-05-05 12:53:29 +0300

    tmod backups fixed

--- serverapi/Managers/Backup/TmodBackupManager.cs
+++ serverapi/Managers/Backup/TmodBackupManager.cs
@@ -18,14 +18,10 @@ public class TmodBackupManager : IBackupManager
         var response = await ContainerHandler.client.Containers.CreateContainerAsync(new CreateContainerParameters
         {
             Image = "alpine",
-            Cmd = ["sh", "-c", $"tar -cf /backup/{backupName}.tar /source/Worlds/{save}.wld /source/Worlds/{save}.twld"],
+            Cmd = ["sh", "-c", $"tar -cf /backup/{backupName}.tar /source/tModLoader/Worlds/{save}.wld /source/tModLoader/Worlds/{save}.twld"],
             HostConfig = new HostConfig
             {
-                Binds =
-            [
-                $"{serverPath}/tModLoader/Worlds:/source/Worlds",
-                $"{backupPath}:/backup"
-            ],
+                Binds = [$"{serverPath}:/source", $"{backupPath}:/backup"],
                 AutoRemove = true
             }
         });
@@ -56,33 +52,28 @@ public class TmodBackupManager : IBackupManager
     {
         string serverPath = PathManager.GetServerPath(serverName);
         string backupPath = PathManager.GetBackupPath(serverName);
+
+        string worldPath = Path.Combine(serverPath, "tModLoader", "Worlds");
         string backupFile = Path.Combine(backupPath, backupName);
 
-        if (!File.Exists(backupFile)) throw new Exception($"Backup '{backupName}' not found.");
+        if (!File.Exists(backupFile))
+            throw new Exception($"Backup not found: {backupFile}");
 
         await ContainerHandler.Stop(serverName, new CancellationToken());
 
         var response = await ContainerHandler.client.Containers.CreateContainerAsync(new CreateContainerParameters
         {
             Image = "alpine",
-            Cmd = ["sh", "-c", $"tar -xf /backup/{backupName} -C /source/ --strip-components=1"],
+            Cmd = ["sh", "-c", $"tar -xf /backup/{backupName} -C /source --strip-components=1"],
             HostConfig = new HostConfig
             {
-                Binds =
-                [
-                    $"{serverPath}:/source",
-                $"{backupPath}:/backup"
-                ],
+                Binds = [$"{serverPath}:/source", $"{backupPath}:/backup"],
                 AutoRemove = true
             }
         });
 
         await ContainerHandler.client.Containers.StartContainerAsync(response.ID, new ContainerStartParameters());
         await ContainerHandler.client.Containers.WaitContainerAsync(response.ID);
-
-        ContainerListResponse? container = await ContainerHandler.GetByName(serverName, new CancellationToken());
-        if (container != null)
-            await ContainerHandler.client.Containers.StartContainerAsync(container.ID, new ContainerStartParameters());
     }
 
     public async Task Delete(string serverName, string backupName)
